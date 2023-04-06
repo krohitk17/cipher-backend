96b42c70fc3dcae40ad01931087257d2ecebe968
Rohit Kumar <rkumar_be20@thapar.edu>
2023-04-07 00:41:29 +0530

    fixed Search By Email api

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,8 @@ const updateUser = require("../controllers/updateUser");
 const verifyToken = require("../middlewares/verifyToken");
 const getUser = require("../controllers/getUser");
 const getUserByEmail = require("../controllers/getUsersByEmail");
+const getUsersByName = require("../controllers/getUsersByName");
+const { BadRequestError } = require("../errors");
 
 router.get("/", verifyToken, async (req, res, next) => {
   console.log("CALL USER GET REQUEST");
@@ -19,12 +21,32 @@ router.get("/", verifyToken, async (req, res, next) => {
   next();
 });
 
-router.get("/?email=", verifyToken, async (req, res, next) => {
-  console.log("CALL USER EMAIL QUERY GET REQUEST");
+router.get("/email", verifyToken, async (req, res, next) => {
+  console.log("CALL USER EMAIL GET REQUEST");
   try {
-    const email = req.query.email;
+    const email = req.header("email");
+    if (email == null) {
+      throw new BadRequestError("Email Header Not Found");
+    }
     const user = await getUserByEmail(email);
     res.status(201).json(user);
+    console.log("USER: ", user);
+  } catch (error) {
+    next(error);
+  }
+  next();
+});
+
+router.get("/name/:page", verifyToken, async (req, res, next) => {
+  console.log("CALL USER NAME GET REQUEST");
+  try {
+    const name = req.header("name");
+    if (name == null) {
+      throw new BadRequestError("Name Header Not Found");
+    }
+    const page = req.params.page;
+    const users = await getUsersByName(name, page);
+    res.status(201).json(users);
   } catch (error) {
     next(error);
   }
